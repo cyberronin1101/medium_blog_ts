958d21a9e4226035f67778e2f301b8130136ffaf
cyberronin1101 <cyberronin1101@gmail.com>
2022-10-22 14:57:56 +0600

    add api service

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,10 +1,61 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 
 const BASE_URL = "https://conduit.productionready.io/api";
 
+type optionsType = {
+  limit?: number;
+  offset?: number;
+  tag?: string;
+  [key: string]: string | number | undefined;
+};
+
+type doFetchType = (
+  options?: optionsType,
+  data?: Object
+) => Promise<AxiosResponse>;
+
+const doFetch = async (url: string, data?: Object) => {
+  let token = localStorage.getItem("token");
+
+  if (token && data) {
+    data = {
+      ...data,
+      ...{
+        headers: {
+          authorization: `Token ${token}`,
+        },
+      },
+    };
+  }
+
+  return await axios(url, data);
+};
+
+const updateUrl = (path: string, urlExtends?: optionsType): string => {
+  if (!urlExtends) {
+    return BASE_URL + path;
+  }
+
+  const pathParts = Object.keys(urlExtends).map(
+    (key) => `${key}=${urlExtends[key]}`
+  );
+
+  return BASE_URL + path + `?${pathParts.join("&")}`;
+};
+
 class ApiService {
-  getTag = async () => {
-    return await axios(BASE_URL + "/tags");
+  getTag: doFetchType = async () => {
+    let path = "/tags";
+    return await doFetch(updateUrl(path));
+  };
+
+  getFeed: doFetchType = async (options) => {
+    let path = "/articles";
+    return await doFetch(updateUrl(path, options));
+  };
+
+  getUser: doFetchType = async (options, data) => {
+    return await doFetch(updateUrl("/user"), data);
   };
 }
 
